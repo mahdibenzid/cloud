74d17d0f5b91ba4a8eb4fdfc0d40bec8b909330a
mahdibenzid <etudiant.benzid.mahdi@uvt.tn>
2023-12-01 02:16:56 +0100

    ok

--- Portfolio-master/Portfolio-master/src/Data/ProjectsData.js
+++ Portfolio-master/Portfolio-master/src/Data/ProjectsData.js
@@ -1,7 +1,7 @@
 const DUMMY_PROJECTS=[
     {
         projectTitle:'PFE',
-        description: 'pour lire le rapport, merci de click dessus.',
+        description: 'pour lire le rapport, merci de click dessus .',
         sourceLink:'https://www.youtube.com/watch?v=DDrqX4h9Mp4',
         lastUpdated:'Janvier  2024'
     },
